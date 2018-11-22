14ed58acf44def9538c3baa456c235b69216f760
Alexander Kayuda <alex.kaes@gmail.com>
2018-11-22 14:49:40 -0500

    initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+const publicPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicPath));
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, (request, response) => {
+
+});
